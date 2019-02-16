0eb2be81f2dd1978f6dc3185ec8c48196a004078
HazelLiang <crocodilestear@gmail.com>
2019-02-16 19:11:54 +0800

    webpack: babel-loader setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 //global app controller
 
 import x from "./test";
+
 console.log(`i am imported ${x} from another module called test.js`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+// https://webpack.js.org/concepts/
+// https://blog.johnwu.cc/article/webpack-4-sass-to-css.html
 const path = require("path");
+//// 載入轉存 html 檔案的套件
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/js/index.js",
+    entry: ['@babel/polyfill', './src/js/index.js'],
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "js/bundle.js"
@@ -14,6 +17,17 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
-        })
+        }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+        ]
+    }
 };
